87cc7106b0b1d1a0ba222d609a316b846fb2b050
mahdikarami77 <mahdikarami77@ut.ac.ir>
2019-04-18 14:32:46 +0430

    fix bug in getusers in HOme page

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ export default class Home extends React.Component {
 
     componentDidMount() {
         new ProjectDispatcher().getProjects(this);
-        new UserDispatcher().getUsers(this);
+        new UserDispatcher().gerUsers(this);
     }
 
     render() {
