5eb42a824e12b641ade78b14b2743492e96b82c0
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-04-18 13:55:45 +0430

    Refactor on projectDispatcher

--- src/req_dispatcher/project_dispatcher.js
+++ src/req_dispatcher/project_dispatcher.js
@@ -2,41 +2,6 @@ import axios from 'axios';
 import * as ConfigManager from '../config/const.js';
 
 export default class ProjectDispatcher {
-    getdata(args) {
-        axios.get('http://172.30.49.41:8084/Proj_IE/project')
-            .then(function (response) {
-                // console.log("salam");
-                // console.log(response);
-
-                return response;
-            }
-                // .then(
-                // response =>
-                // args.setState ({
-                //     currentID: JSON.parse(response.data.details).currentID,
-                //     projects: JSON.parse(response.data.message),
-                // })
-            )
-            .catch(
-                function (error) {
-                    console.log(error.stack);
-                }
-            )
-        // axios.get('http://localhost:8080/Proj_IE/user')
-        // .then(
-        //     response =>
-        //     this.setState ({
-        //         users: response.data
-        //     })
-        // )
-        // .catch(
-        //     function(error) {
-        //         console.log(error.stack);
-        //     }
-        // )
-    }
-
-
 
     getProject(procjetID, obj) {
         axios.get(ConfigManager.SERVER_ADDRESS + 'project/' + procjetID)
