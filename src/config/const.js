c657663a943f5ad064e87ec4328883df405230f1
mahdikarami77 <mahdikarami77@ut.ac.ir>
2019-05-26 21:53:29 +0430

    chagen url of server

--- src/config/const.js
+++ src/config/const.js
@@ -1,3 +1,3 @@
 // export const SERVER_ADDRESS = "http://127.0.0.1:8080/Proj_IE/"
-export const SERVER_ADDRESS = "http://127.0.0.1:8080/"
+export const SERVER_ADDRESS = "http://127.0.0.1:8080/Proj_IE-1.0-SNAPSHOT/"
 export const CLIENT_ADDRESS = "http://127.0.0.1:3000"
