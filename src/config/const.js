0d9873dde8ce7159929dd2c734748db5ee92dcef
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-05-26 15:19:20 +0430

    Amend server and internal server link in const file

--- src/config/const.js
+++ src/config/const.js
@@ -1,2 +1,3 @@
-export const SERVER_ADDRESS = "http://127.0.0.1:8080/Proj_IE/"
+// export const SERVER_ADDRESS = "http://127.0.0.1:8080/Proj_IE/"
+export const SERVER_ADDRESS = "http://127.0.0.1:8080/"
 export const CLIENT_ADDRESS = "http://127.0.0.1:3000"
